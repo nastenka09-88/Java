5e2267f7dca3021b9d865f5f246431da1aeaa562
nastenka09-88 <68924655+nastenka09-88@users.noreply.github.com>
2021-06-08 09:52:55 +0300

    Add files via upload

---
+++ JavaFundamentals/MainTask/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner userInputScanner = new Scanner(System.in);
+        System.out.println("What's your name? ");
+        String userInputName = userInputScanner.nextLine();
+        System.out.println("Hello " + userInputName + "!");
+    }
+}
